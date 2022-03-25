20de3da63f6767067e45bd0993de6018ed5041a2
Maxime <monillon@outlook.fr>
2022-03-25 20:14:48 +0100

    On retire le showDetails pour pathologie

--- Modules/Administration/Listing/Pathologie/AdminListPathologieModel.java
+++ Modules/Administration/Listing/Pathologie/AdminListPathologieModel.java
@@ -57,41 +57,4 @@ public class AdminListPathologieModel {
         }
     }
 
-    /**
-     * Permet d'aller chercher en BDD si le tissu est lié à un ou plusieurs prélèvements et d'afficher un message.
-     * @param leTissu le tissu à rechercher
-     * @param affichage le lieu de l'affichage pour le message d'erreur
-     * @param detailsPrelev le lieu d'affiche de la liste des prélèvements
-     */
-    public void showDetails(Tissue leTissu, Text affichage, Text detailsPrelev) throws IOException {
-        //TODO: reste à faire le show details
-        ArrayList<JSONObject> retour = DataBase.getPrelevementLieTissu(leTissu.getIdTissue());
-
-        if (retour.get(0).isNull("message")) {
-            // Le tissu est lié à des prélèvements afficher les prélèvements
-            affichage.setText("Le tissu est lié à " + retour.size() + " prélèvements, il ne peut pas être supprimé");
-            ArrayList<JSONObject> details = DataBase.getSujetLiePrelevment(leTissu.getIdTissue());
-
-            // permet d'afficher les 5 premiers sujets où le tissu est utilisé.
-            int parcours;
-            if (details.size() >= 5 ) {
-                parcours = 5;
-            } else {
-                parcours = details.size();
-            }
-
-            String texte = "";
-            for (int i = 0; i < parcours; i++) {
-                texte += details.get(i).getString("CODE_SUJET") + " ";
-            }
-            detailsPrelev.setText("Ce tissu est utilisé pour les sujets : " + texte);
-
-        } else {
-            // pas de prélèvement lié
-            affichage.setText("Ce tissu n'est pas lié à des prélèvements, il peut être supprimé");
-            detailsPrelev.setText("");
-        }
-
-    }
-
 }
